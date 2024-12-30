0521bea00ac7798a3fb277c517cbff422f40e031
YChaoCode <yichaowang639@gmail.com>
2024-12-30 17:11:24 +0800

    解決ReferenceError: document is not defined的問題

--- cafemate_app/src/getCookies.ts
+++ cafemate_app/src/getCookies.ts
@@ -1,14 +1,20 @@
 export default function getCookie(name: string) {
+    if (typeof window === "undefined") {
+      // 如果是伺服器端，直接返回空值
+      return null;
+    }
+  
     let cookieValue = null;
     if (document.cookie && document.cookie !== "") {
-        const cookies = document.cookie.split(";");
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            if (cookie.startsWith(name + "=")) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
+      const cookies = document.cookie.split(";");
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.startsWith(name + "=")) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
         }
+      }
     }
     return cookieValue;
-}
+  }
+  
